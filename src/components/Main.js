437bed06d772ac42a3c74e24c8508821f34f6afb
Ian Renyard <ian@renyard.net>
2019-09-27 00:02:53 +0100

    Enabled Netlify forms for contact page

--- src/components/Main.js
+++ src/components/Main.js
@@ -132,7 +132,17 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form
+            name="contact"
+            method="post"
+            action="#"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <div hidden>
+              <input type="text" name="bot-field" value="" />
+            </div>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
